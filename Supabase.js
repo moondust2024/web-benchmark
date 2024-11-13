5fa48c60ea2e14f99bd13e19cc197d8d6540c656
moondust2024 <157769827+moondust2024@users.noreply.github.com>
2024-11-13 11:34:30 +0800

    update supabase

--- Supabase.js
+++ Supabase.js
@@ -1,17 +1,9 @@
-function loadScript(url, callback) {
-    const script = document.createElement('script');
-    script.src = url;
-    script.onload = callback;
-    document.head.appendChild(script);
-}
-  
-loadScript('https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2', () => {
-    if (typeof supabase === 'undefined') {
-        console.error('Supabase library is not loaded');
-    } else {
+
+import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2'
+
         const supabaseUrl = 'https://skbwsugoaatergloenlq.supabase.co';
         const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrYndzdWdvYWF0ZXJnbG9lbmxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzOTc0MjIsImV4cCI6MjA0Njk3MzQyMn0.fvMKNXkFPCvdjLZsQ3VHOfIog3uaW6mfJ5KuVoD-lKQ';
-        const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+        const supabase = createClient(supabaseUrl, supabaseKey);
 
         async function saveScore() {
         const cpumodel = document.getElementById('model').value;
@@ -30,5 +22,3 @@ loadScript('https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2', () => {
             console.log('Score saved:', data);
         }
         }
-    }
-});
