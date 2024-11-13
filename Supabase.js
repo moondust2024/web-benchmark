90c0b3d368990cd7f49cd49474cf6fbe3bfcf856
moondust2024 <157769827+moondust2024@users.noreply.github.com>
2024-11-13 11:38:09 +0800

    update supabase

--- Supabase.js
+++ Supabase.js
@@ -1,24 +1,28 @@
 
 import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2'
 
+document.addEventListener('DOMContentLoaded', function() {
         const supabaseUrl = 'https://skbwsugoaatergloenlq.supabase.co';
         const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrYndzdWdvYWF0ZXJnbG9lbmxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzOTc0MjIsImV4cCI6MjA0Njk3MzQyMn0.fvMKNXkFPCvdjLZsQ3VHOfIog3uaW6mfJ5KuVoD-lKQ';
         const supabase = createClient(supabaseUrl, supabaseKey);
 
         async function saveScore() {
-        const cpumodel = document.getElementById('model').value;
-        const score = document.getElementById('floatscore').value;
-        const scoretext = document.getElementById('score').value;
+            const cpumodel = document.getElementById('model').value;
+            const score = document.getElementById('floatscore').value;
+            const scoretext = document.getElementById('score').value;
 
-        const { data, error } = await supabase
-            .from('CPURank')
-            .insert(
-            { id: 'id', cpumodel: cpumodel, score: parseFloat(score) , scoretext: scoretext }
-            );
+            const { data, error } = await supabase
+                .from('CPURank')
+                .insert(
+                { id: 'id', cpumodel: cpumodel, score: parseFloat(score) , scoretext: scoretext }
+                );
 
-        if (error) {
-            console.error('Error saving score:', error); 
-        } else {
-            console.log('Score saved:', data);
+            if (error) {
+                console.error('Error saving score:', error); 
+            } else {
+                console.log('Score saved:', data);
+            }
         }
-        }
+        
+        window.saveScore = saveScore;
+});
